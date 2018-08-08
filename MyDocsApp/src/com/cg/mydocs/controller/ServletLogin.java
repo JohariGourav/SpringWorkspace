6821e869e29835dd75dbe39a8f5be5f5729add14
Gourav Johari <41551799+JohariGourav@users.noreply.github.com>
2018-08-08 08:50:04 -0400

    mydocs app servlet jsp

---
+++ MyDocsApp/src/com/cg/mydocs/controller/ServletLogin.java
@@ -0,0 +1,40 @@
+package com.cg.mydocs.controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+
+@WebServlet("/ServletLogin")
+public class ServletLogin extends HttpServlet {
+	
+    public ServletLogin() {
+        
+    }
+
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+			
+		System.out.println("get run");
+		HttpSession session = request.getSession();
+		
+		String userName = request.getParameter("userName");
+		String  password = request.getParameter("password");
+		if(userName == "admin" && password == "admin") {
+			session = request.getSession();
+			session.setAttribute("username", userName);
+			response.sendRedirect("success.jsp");
+		}
+	}
+
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
