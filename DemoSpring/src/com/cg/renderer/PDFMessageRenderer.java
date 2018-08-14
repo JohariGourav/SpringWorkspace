77495da2c1054f67fa061eda963ef01c305656e6
Gourav Johari <41551799+JohariGourav@users.noreply.github.com>
2018-08-14 08:13:48 -0400

    added demospring

--- DemoSpring/src/com/cg/renderer/PDFMessageRenderer.java
+++ DemoSpring/src/com/cg/renderer/PDFMessageRenderer.java
@@ -4,6 +4,16 @@ import com.cg.provider.MessageProvider;
 
 public class PDFMessageRenderer implements MessageRenderer {
 	private MessageProvider provider;
+	
+	
+	public PDFMessageRenderer(MessageProvider provider) {
+	System.out.println("Arg Ctor Created for PDF");
+		this.provider = provider;
+	}
+
+	public PDFMessageRenderer() {
+	System.out.println("No arg Ctor created for PDF");
+	}
 
 	@Override
 	public void render() {
